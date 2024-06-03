b0c53dd4e27687e7d744eb2b235c4e2fbd2279b5
Ayush Sharma <ayush22@navgurukul.org>
2024-06-03 22:50:27 +0530

    HVA javaScript Question

---
+++ Basic_Array.js
@@ -0,0 +1,8 @@
+// Define a variable named colors and assign it an array containing strings representing different colors (e.g., "red", "green", "blue").
+
+// Log the first element of the colors array to the console.
+
+let color = ["Red" , "Green" , "Blue"]
+color[1] = "Yellow"
+color.push("Black")
+console.log(color);
