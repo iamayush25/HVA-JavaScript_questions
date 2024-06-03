18bdca53bdcb3a22ae5c72346486d01909029433
Ayush Sharma <ayush22@navgurukul.org>
2024-06-03 22:50:45 +0530

    HVA javaScript Question

---
+++ Loop_on_Array.js
@@ -0,0 +1,24 @@
+// Use a for loop to iterate over the colors array and log each color to the console.
+
+let color = ["Red" , "Green" , "Blue"]
+for (let i = 0; i < color.length; i++) {
+    console.log(color[i]);
+    
+}
+
+// Use a while loop to iterate over the colors array and log each color to the console.
+
+let color1 = ["Red" , "Green" , "Blue"]
+let i = 0;
+while (i < color1) {
+    console.log(color1[i]);
+    i++
+}
+
+// Use a for...of loop to iterate over the colors array and log each color to the console.
+
+let color2 = ["Red" , "Green" , "Blue"]
+for (const i of color2) {
+    console.log(i);
+}
+
