7c053fb6a4bfe162c4ba3a28174ffc7d2b84f245
Ayush Sharma <ayush22@navgurukul.org>
2024-06-05 23:12:40 +0530

    HVA Javascript question

---
+++ Array-as-an-Object.js
@@ -0,0 +1,22 @@
+// Use the typeof operator to check the type of colors array and console it.
+
+//Log the length property of the colors array to the console to see the number of elements in the array.
+
+// Use the push method to add another color to the end of the colors array.
+//Use the pop method to remove the last color from the colors array.
+
+// Use the indexOf method to find the index of a specific color (e.g., "blue") in the colors array.
+
+// Add a property to the colors array called owner and set its value to your name. Log the colors array to see if the owner property is added.
+
+let color = ["Red" , "Green" , "Blue"]
+console.log(typeof color);
+console.log(color.length);
+color.push("Yellow")
+console.log(color);
+color.pop()
+console.log(color);
+console.log(color.indexOf("Red"));
+
+
+
